3c4dc15415b74309b66aee7581a56419fe26a45c
limjoonhyuk <144293629+limjoonhyuk@users.noreply.github.com>
2023-09-07 10:49:11 +0900

    피라미드

--- Project3/pro.cpp
+++ Project3/pro.cpp
@@ -1,9 +1,22 @@
 
 #include <stdio.h>
 
-int main(void)
-{
-printf("hello world");
+int main(void){
+	int n, i, j;
+	scanf_s("%d", &n);
+	printf("%d층 피라미드\n",n);
+	for (i = 1; i <= n; i++) {
+		for (j = 1; j <= n - i; j++) {
+			printf(" ");
+
+		}
+		for (int k = 1; k <= 2 * i - 1; k++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
 
 return 0;
 }
