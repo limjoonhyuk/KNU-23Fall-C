9563b7af4ed734b59316c9cb6637939df84adc10
kimjuock <144293524+kimjuock@users.noreply.github.com>
2023-09-14 11:58:16 +0900

    for

---
+++ 20230914/lecture 9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void)
+
+{
+	int i;
+	int sum = 0;
+	int val= 1;
+
+	for (i = 0; i < 10; i+=2)
+	{
+		printf("i=%d val=%d sum=%d\n", i, val, sum);
+
+		sum += val;
+		val +=1;
+
+	}
+	printf("sum = %d\n", sum);
+	return 0;
+}
