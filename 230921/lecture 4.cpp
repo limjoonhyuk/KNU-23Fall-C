08ab2542c1c6a5340912756acd7e004d8fba207c
jimin7312 <jimin7312@naver.com>
2023-09-21 11:30:26 +0900

    원주율

--- 230921/lecture 4.cpp
+++ 230921/lecture 4.cpp
@@ -1,18 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
 
-int fact(int num)
+int main(void)
 {
-	if (num == 0) return 1;
+    double x = 0;
+    double y = 0;
 
-	return num * fact(num - 1);
-}
+    double count = 0, circle = 0;
 
-int main()
-{
-	int num;
-	scanf_s("%d", &num);
+    int percent = 0;
 
-	printf("%d\n", fact(num));
+    srand(time(NULL));
 
-	return 0;
+    while (count < 10000) {
+        x = (double)rand() / (double)RAND_MAX;
+        y = (double)rand() / (double)RAND_MAX;
+        count++;
+
+        if ((x * x) + (y * y) <= 1) {
+            circle++;
+        }
+
+        percent = count / 100;
+
+        printf("%d%%진행.. 원주율 : %lf ", percent, (circle / count) * 4);
+        for (int i = 0; i < percent; i += 5)
+        {
+            printf("=");
+        }
+        printf("\n");
+
+    }
+
+    return 0;
 }
