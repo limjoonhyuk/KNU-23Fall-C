156d81e0fa686c04a0ab3be67d349f2fcd64fe00
limjoonhyuk <junhyeok9064@naver.com>
2023-10-12 10:53:34 +0900

    pointer swap

---
+++ test/lecture 5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+void swap(int* pa, int* pb)
+{
+	int c = *pa;
+	*pa = *pb;
+	*pb = *pa;
+}
+
+int main(void)
+{
+
+	int a = 10;
+	int b = 565;
+
+	printf("%d %d\n", a, b);
+	swap(a, b);
+	printf("%d %d\n", a, b);
+
+	return 0;
+
+
+}
